1d5528336760245ff31e613cf56d629ced4e004a
Kelvin <149409560+KJR-767@users.noreply.github.com>
2023-11-29 10:41:55 +0000

    Add files via upload

---
+++ Caesar Cipher.py
@@ -0,0 +1,7 @@
+print ('''Here i have wrote a simple Caesar Cipher that will Encrypt and  
+Decrypt a message, there is also an option for the user to learn how a 
+Caesar Cipher works and also learn some history about the Cipher''')
+
+# Below is a string that contains each letter of the alphabet.
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
