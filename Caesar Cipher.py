95201d20bbfea6e04412c1703776ebe848a6a79a
KJR-767 <149409560+KJR-767@users.noreply.github.com>
2023-11-29 10:52:30 +0000

    V0.1

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -5,3 +5,13 @@ Caesar Cipher works and also learn some history about the Cipher''')
 # Below is a string that contains each letter of the alphabet.
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+while True:
+    print('Would you like to (e)ncrypt or (d)ecrypt a message')
+    response = input('> ').lower()
+    if response.startswith('e'):
+        mode = 'encrypt'
+        break
+    elif responce.startswith('d'):
+        mode = 'decrypt'
+        break
+    print('Please enter the letter e or d.')
